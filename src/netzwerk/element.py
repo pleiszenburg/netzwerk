38c31dc860d64d7c4d6dd5aac95af1664fe216d6
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-08-27 12:41:38 +0200

    isolated const, element and uebergang

---
+++ src/netzwerk/element.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+NETZWERK
+Computer network renderer
+https://github.com/pleiszenburg/netzwerk
+
+	src/netzwerk/element.py: Basis-Klasse für alle Netzwerk-Gegenstände
+
+	Copyright (C) 2017 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/netzwerk/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from .const import *
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# KLASSEN
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Basis-Klasse: Alles ist eine Element, hat einen Namen und Parameter
+class element(object):
+
+	def __init__(self, parameter):
+
+		self.p = parameter
+		self.svg = {}
+
+
+	def rendere_svg(self, parameter):
+
+		raise NotImplementedError
+
+
+	def ersetze_parameter(self, vorlage, parameter):
+
+		for key in list(parameter.keys()):
+
+			vorlage = vorlage.replace('{{' + key + '}}', str(parameter[key]))
+
+		return vorlage
+
+
+	def schreibe_svg(self, dateiname):
+
+		f = open(dateiname, 'w')
+		f.write(self.svg[KEY_SVG])
+		f.close()
+
+
+	def __sortiere_liste_von_elementen__(self, liste_elemente, schluessel):
+
+		namen = []
+		for gegenstand in liste_elemente:
+			namen.append(gegenstand.p[schluessel])
+
+		namen.sort()
+
+		neue_liste = []
+		for name in namen:
+			for gegenstand in liste_elemente:
+				if name == gegenstand.p[schluessel]:
+					neue_liste.append(gegenstand)
+					break
+
+		liste_elemente[:] = []
+		for gegenstand in neue_liste:
+			liste_elemente.append(gegenstand)
+
+
+	def __vervollstaendige_svg__(self, svg, globaler_kopf = False):
+
+		if globaler_kopf:
+
+			svg.insert(0, '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 %d %d" width="%d" height="%d">' % (
+				self.svg[KEY_BREITE], self.svg[KEY_HOEHE], self.svg[KEY_BREITE], self.svg[KEY_HOEHE]
+				))
+			svg.insert(0, '<?xml version="1.0" encoding="UTF-8"?>\n')
+
+		else:
+
+			if KEY_X in self.svg.keys() and KEY_Y in self.svg.keys():
+				svg.insert(0, '<svg x="%d" y="%d" width="%d" height="%d">' % (
+					self.svg[KEY_X], self.svg[KEY_Y], self.svg[KEY_BREITE], self.svg[KEY_HOEHE]
+					))
+			else:
+				svg.insert(0, '<svg x="{{' + KEY_X + '}}" y="{{' + KEY_Y + '}}" width="%d" height="%d">' % (
+					self.svg[KEY_BREITE], self.svg[KEY_HOEHE]
+					))
+
+		svg.append('</svg>')
+
+		self.svg[KEY_SVG] = ''.join(svg)
+
+
+	def __fixiere_element__(self, position):
+
+		self.svg.update(position)
+		self.svg[KEY_SVG] = self.ersetze_parameter(self.svg[KEY_SVG], position)
+
+
+	def __korrigiere_positionen__(self):
+
+		raise NotImplementedError
+
+
+	def __zeichne_schnittstellen__(self, svg):
+
+		raise NotImplementedError
+
+
+	def __ipv4_zu_text__(self, wert):
+
+		return '.'.join([('%d' % seg) for seg in wert])
+
+
+	def __ipv6_zu_text__(self, wert):
+
+		raise NotImplementedError
+
+
+	def __mac_zu_text__(self, wert):
+
+		return ':'.join([('%02x' % seg) for seg in wert])
---
+++ src/netzwerk/uebergang.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+NETZWERK
+Computer network renderer
+https://github.com/pleiszenburg/netzwerk
+
+	src/netzwerk/uebergang.py: Übergänge wie Dosen
+
+	Copyright (C) 2017 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/netzwerk/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from .const import *
+from .element import element
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# KLASSEN
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Basis für Dosen, Löcher in den Wänden, Patchfelder ...
+class uebergang(element):
+
+	def __init__(self, parameter):
+
+		super().__init__(parameter)
+
+		self.schnittstellen = {} # Haben alle Verbindungen Schnittstellen oder nur Ethernet-Dosen?
+
+		# Standart-Parameter setzen, falls nichts vorhanden
+		if KEY_NAME not in self.p.keys():
+			if KEY_MASKE not in self.p.keys():
+				self.p[KEY_MASKE] = '%02d'
+			if KEY_PREFIX not in self.p.keys():
+				self.p[KEY_PREFIX] = ''
+			if KEY_SUFFIX not in self.p.keys():
+				self.p[KEY_SUFFIX] = ''
+			self.p[KEY_NAME] = self.p[KEY_PREFIX] + (self.p[KEY_MASKE] % self.p[KEY_NUMMER]) + self.p[KEY_SUFFIX]
+		if KEY_HIERARCHIEEBENE not in self.p.keys():
+			self.p[KEY_HIERARCHIEEBENE] = KEY_RAUM
+		if KEY_SCHNITTSTELLEN not in self.p.keys():
+			self.p[KEY_SCHNITTSTELLEN] = {}
+		if KEY_ETHERNET not in self.p[KEY_SCHNITTSTELLEN].keys():
+			self.p[KEY_SCHNITTSTELLEN][KEY_ETHERNET] = []
+
+		# Schnittstellen zu Parametern hinzufuegen
+		if self.p[KEY_TYP] == KEY_DOSE:
+			if self.p[KEY_HIERARCHIEEBENE] == KEY_RAUM:
+				verbindungen = {KEY_A: KEY_KLEMME, KEY_B: KEY_STECKER}
+			else:
+				verbindungen = {KEY_A: KEY_STECKER, KEY_B: KEY_KLEMME}
+		elif self.p[KEY_TYP] == KEY_LOCH:
+			verbindungen = {KEY_A: KEY_LOCH, KEY_B: KEY_LOCH}
+		elif self.p[KEY_TYP] == KEY_VORBEREITET:
+			verbindungen = {KEY_A: KEY_VORBEREITET}
+		else:
+			raise
+		verbindungen_schluessel = list(verbindungen.keys())
+		verbindungen_schluessel.sort()
+		for index, verbindung in enumerate(verbindungen_schluessel):
+			self.p[KEY_SCHNITTSTELLEN][KEY_ETHERNET].append({
+				KEY_NAME: verbindung,
+				KEY_NUMMER: index,
+				KEY_PASSIV: True,
+				KEY_ELTERN: self.p[KEY_TYP],
+				KEY_VERBINDUNG: verbindungen[verbindung]
+				})
+
+		# Schnittstellen erstellen
+		for ethernet_port in self.p[KEY_SCHNITTSTELLEN][KEY_ETHERNET]:
+			neue_schnittstelle = schnittstelle_ethernet(ethernet_port)
+			self.schnittstellen[neue_schnittstelle.p[KEY_NAME]] = neue_schnittstelle
+
+
+	def __zeichne_schnittstellen__(self, svg):
+
+		for schluessel_schnittstelle in list(self.schnittstellen.keys()):
+			self.schnittstellen[schluessel_schnittstelle].__zeichne_schnittstelle__(svg)
+
+
+	def __korrigiere_positionen__(self):
+
+		for schluessel_schnittstelle in list(self.schnittstellen.keys()):
+			ding = self.schnittstellen[schluessel_schnittstelle]
+			for dimension in [KEY_X, KEY_Y]:
+				ding.svg[dimension] += self.svg[dimension]
+			ding.__korrigiere_positionen__()
+
+
+	def __rendere_svg__(self, parameter):
+
+		svg = []
+		self.svg = parameter
+
+		# Hintergrund
+		svg.append(
+			'<rect x="%d" y="%d" width="%d" height="%d" fill="%s" />' % (
+				0, 0, PARAM_DOSE_BREITE, PARAM_DOSE_HOEHE, parameter[KEY_HINTERGRUNDFARBE]
+				)
+			)
+
+		# Beschriftungen ausgeben
+		svg.append(
+			'<text x="%d" y="%d" font-family="%s" font-size="%d" fill="%s" dominant-baseline="middle" text-anchor="middle">%s</text>' % (
+				PARAM_DOSE_BREITE / 2, PARAM_DOSE_HOEHE / 2, PARAM_TEXT_FONT, PARAM_ETHERNET_SCHRIFTGROESSE, PARAM_TEXT_FARBE, self.p[KEY_NUMMER]
+				)
+			)
+
+		# Schnittstellen 'darstellen' und positionieren
+		if self.p[KEY_HIERARCHIEEBENE] == KEY_RAUM:
+			position = {KEY_X: [0, PARAM_DOSE_BREITE], KEY_Y: [PARAM_DOSE_HOEHE / 2, PARAM_DOSE_HOEHE / 2], KEY_ORIENTIERUNG: [KEY_LINKS, KEY_RECHTS]}
+		else:
+			position = {KEY_X: [PARAM_DOSE_BREITE / 2, PARAM_DOSE_BREITE / 2], KEY_Y: [0, PARAM_DOSE_HOEHE], KEY_ORIENTIERUNG: [KEY_OBEN, KEY_UNTEN]}
+		schnittstellen_keys = list(self.schnittstellen.keys())
+		schnittstellen_keys.sort()
+		for index, schnittstelle in enumerate(schnittstellen_keys):
+			self.schnittstellen[schnittstelle].rendere_svg({
+				KEY_X: position[KEY_X][index],
+				KEY_Y: position[KEY_Y][index],
+				KEY_ORIENTIERUNG: position[KEY_ORIENTIERUNG][index]
+				})
+			svg.append(self.schnittstellen[schnittstelle].svg[KEY_SVG])
+
+		# Parameter zur Rückgabe vorbereiten
+		self.svg.update({
+			KEY_BREITE: PARAM_DOSE_BREITE,
+			KEY_HOEHE: PARAM_DOSE_HOEHE
+			})
+
+		# SVG fertig machen
+		self.__vervollstaendige_svg__(svg)
+
+
+# Dosen mit Steckern und Klemmen
+class uebergang_dose(uebergang):
+
+	def __init__(self, parameter):
+
+		super().__init__(parameter)
+
+
+	def rendere_svg(self, parameter):
+
+		parameter[KEY_HINTERGRUNDFARBE] = PARAM_ETHERNET_FARBE
+		self.__rendere_svg__(parameter)
+
+
+# Loch in der Wand
+class uebergang_loch(uebergang):
+
+	def __init__(self, parameter):
+
+		super().__init__(parameter)
+
+
+	def rendere_svg(self, parameter):
+
+		parameter[KEY_HINTERGRUNDFARBE] = PARAM_LOCH_FARBE
+		self.__rendere_svg__(parameter)
+
+
+# Vorbereitete Dose, beispielsweise durch Kabel in Decke
+class uebergang_vorbereitet(uebergang):
+
+	def __init__(self, parameter):
+
+		super().__init__(parameter)
+
+
+	def rendere_svg(self, parameter):
+
+		parameter[KEY_HINTERGRUNDFARBE] = PARAM_VORBEREITET_FARBE
+		self.__rendere_svg__(parameter)
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINEN
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Viele Dosen bilden ein Patchfeld - Funktion erstellt Liste von Dosen
+def verbindung_patchfeld(parameter):
+
+	if KEY_MASKE not in parameter.keys():
+		parameter[KEY_MASKE] = '%02d'
+	if KEY_PREFIX not in parameter.keys():
+		parameter[KEY_PREFIX] = ''
+	if KEY_SUFFIX not in parameter.keys():
+		parameter[KEY_SUFFIX] = ''
+
+	dosen = []
+
+	anschluesse = parameter[KEY_ANSCHLUESSE]
+	anschluesse.sort()
+	del parameter[KEY_ANSCHLUESSE]
+	parameter[KEY_TYP] = KEY_DOSE
+
+	for anschluss in anschluesse:
+		parameter[KEY_NUMMER] = anschluss
+		dosen.append(uebergang_dose(copy.deepcopy(parameter)))
+
+	return dosen
